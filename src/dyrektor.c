11dc271ec0d799f5b5e877a32e7f0d79d06a6e03
Se1roon <se1roonm@pm.me>
2025-12-11 11:41:01 +0100

    [Dyrektor] Created basic version of Dyrektor process

---
+++ src/dyrektor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int main(void) {
+	pid_t cpid;
+
+	cpid = fork();
+	switch (cpid) {
+		case -1:
+			fprintf(stderr, "[Dyrektor][main][ERRN] Failed to create child process!\n");
+			exit(1);
+		case 0: // Child
+			if (execl("./fabryka", "./fabryka", NULL) < 0) {
+				fprintf(stderr, "[Dyrektor][main][ERRN] Faield to create Fabryka process!\n");
+				exit(1);
+			}
+			break;
+		default: // Parent
+			printf("Parent\n");
+	};
+	
+	return 0;
+}
+
