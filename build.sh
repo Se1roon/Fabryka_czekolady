fba6edb8f18e2e7be7f5fa54a66f1fa69e50112a
Se1roon <se1roonm@pm.me>
2025-12-17 13:03:25 +0100

    Replaced Makefile with a simple build script for now

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CFLAGS="-Wall -Wextra"
+
+clang -o ./bin/fabryka ./src/fabryka.c $CFLAGS
+clang -o ./bin/dostawcy ./src/dostawcy.c $CFLAGS
+clang -o ./bin/dyrektor ./src/dyrektor.c $CFLAGS
+
+./bin/dyrektor
